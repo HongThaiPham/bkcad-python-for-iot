7988cd4450c1c1e8dd10f0de2034fab1f7e53de5
Pham Hong Thai <hongthaipro@gmail.com>
2022-12-12 21:34:50 +0700

    Buoi 1

--- python-essentials-1/lab-the-print-function-3.py
+++ python-essentials-1/lab-the-print-function-3.py
@@ -1,2 +1,25 @@
-print("Programming","Essentials","in", sep="***", end="...")
+print("Programming", "Essentials", "in", sep="***", end="...")
 print("Python")
+
+print("Hello\nworld")
+# \n: new line
+print("Hello\tworld")
+# \t: \tab
+
+# In ra dòng sau:  Hello world, welcome to "Python"
+# Trong do: s1 = Hello world ; s2 = welcome to "Python"
+
+print("Hello world", "welcome to \"Python\"", sep=", ")
+
+n = 5
+s = "s"
+print("You have {1} apple{0}".format(n, s))
+
+
+hoTen = "Pham Hong Thai"
+noiLamViec = "LHU"
+print("Toi ten la {}; dang lam viec tai \'{}\'".format(hoTen, noiLamViec))
+
+
+price = 5000
+print("{:0,.2f} vnd".format(price))
