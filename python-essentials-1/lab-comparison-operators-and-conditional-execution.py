4ca413d3bfa6f62639dde442827c5adbedd94a1a
Pham Hong Thai <hongthaipro@gmail.com>
2022-12-13 22:40:08 +0700

    B2

---
+++ python-essentials-1/lab-comparison-operators-and-conditional-execution.py
@@ -0,0 +1,7 @@
+name = input("Enter the word: ")
+if name == 'Spathiphyllum':
+    print("Yes - Spathiphyllum is the best plant ever!")
+elif name == 'spathiphyllum':
+    print("No, I want a big Spathiphyllum!")
+else:
+    print("Spathiphyllum! Not [{}]!".format(name))
---
+++ python-essentials-1/lab-essentials-of-the-if-else-statement.py
@@ -0,0 +1,11 @@
+income = float(input("Enter the annual income: "))
+
+if income < 85_528:
+    tax = income * (18 / 100) - 556.2
+else:
+    tax = 14_839.2 + ((income - 85_528) * 32/100)
+
+if tax < 0:
+    tax = 0.0
+tax = round(tax, 0)
+print("The tax is:", tax, "thalers")
